6d43a8f6f81bbe78f17f1c6d84dd6acbcfab583f
isabellaobrien <>
2023-05-01 08:21:41 +0000

    add first part of startQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,52 @@ const startButton = document.getElementById('start-button');
 
 startButton.addEventListener("click", startQuiz);
 
+let intro = document.getElementById('intro');
+
+
 function startQuiz(){
-    console.log("started")
-    startButton.classList.add('hide')
+    console.log("started");
+    startButton.classList.add('hide');
+    intro.classList.add('hide');
 }
+// function countdown(){
+
+// }
+
+// function lifeCount(){
+
+// }
+
+// function shuffle(){
+
+// }
+
+// function displayQuestion(){
+
+// }
+
+// function selectAnswer(){
+
+// }
+
+// function checkAnswer(){
+
+// }
+
+// function incrementScore(){
+
+// }
+
+// function nextQuestion(){
+
+// }
+
+// function gameOver(){
+
+// }
+// function endGame(){
+
+// }
+
+// function resetGame(){
+
